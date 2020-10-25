f15b28d4f8a0ac24b83bdcbcaf19ce7b33d69b94
Rafael Sadowski <rafael@sizeofvoid.org>
2020-10-25 12:43:14 +0100

    Add first kqueue

---
+++ test/unit/kqueue_controller_test.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020 Rafael Sadowski <rafael@sizeofvoid.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <notify-cpp/kqueue.h>
+#include <notify-cpp/kqueue_controller.h>
+
+#include "filesystem_event_helper.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <future>
+#include <iostream>
+
+using namespace notifycpp;
+
+BOOST_FIXTURE_TEST_CASE(shouldNotifyOnMultipleEvents, FilesystemEventHelper)
+{
+    KqueueController notifier = KqueueController();
+
+    Event watchOn = Event::open | Event::close_write;
+
+    notifier.watchFile({testFileOne_, watchOn}).onEvents({Event::open, Event::close_write}, [&](Notification notification) {
+        switch (notification.getEvent()) {
+        case Event::open:
+            promisedOpen_.set_value(notification);
+            break;
+        case Event::close_write:
+            promisedCloseNoWrite_.set_value(notification);
+            break;
+        default:
+            break;
+        }
+    });
+
+    std::thread thread([&notifier]() {
+        notifier.runOnce();
+        notifier.runOnce();
+    });
+
+    openFile(testFileOne_);
+
+    auto futureOpen = promisedOpen_.get_future();
+    auto futureCloseNoWrite = promisedCloseNoWrite_.get_future();
+    BOOST_CHECK(futureOpen.wait_for(timeout_) == std::future_status::ready);
+    BOOST_CHECK(futureOpen.get().getEvent() == Event::open);
+    BOOST_CHECK(futureCloseNoWrite.wait_for(timeout_) == std::future_status::ready);
+    BOOST_CHECK(futureCloseNoWrite.get().getEvent() == Event::close_write);
+    thread.join();
+}
