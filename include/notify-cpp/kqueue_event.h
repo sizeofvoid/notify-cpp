69b2f3e61f120a4421651b35e8b061217fc45dc0
Rafael Sadowski <rafael@sizeofvoid.org>
2020-10-25 12:16:50 +0100

    Fix compile on OpenBSD/kqueue-system

--- include/notify-cpp/kqueue_event.h
+++ include/notify-cpp/kqueue_event.h
@@ -34,8 +34,6 @@ public:
 
     virtual std::uint32_t convertToEvents(const Event) const override;
     virtual std::uint32_t getEvent(const Event) const override;
-    virtual std::vector<Event> getEvents(std::uint32_t) const override;
     virtual Event get(std::uint32_t) const override;
-    virtual std::string toString(std::uint32_t) const override;
 };
 }
